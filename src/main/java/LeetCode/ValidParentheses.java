c8fab57d33648626b99843723e5d8060779722ab
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2019-09-19 15:51:36 -0400

    Roman To Integer

--- src/main/java/LeetCode/ValidParentheses.java
+++ src/main/java/LeetCode/ValidParentheses.java
@@ -1,4 +1,21 @@
 package LeetCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ValidParentheses {
+
+    /**** method 1 revert half of the ****/
+    public static boolean isValid(String s) {
+        // the length of string s has to be a even number
+        if (s == null || s.length()%2 != 0) return false;
+
+
+
+    }
+
+    public static void main(String[] args) {
+        String s = "([{";
+        System.out.println(isValid(s));
+    }
 }
