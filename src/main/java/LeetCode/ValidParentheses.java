93b32bd9cc01e8f21bbb3aadc4564d45da7a47bd
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2019-09-22 15:06:45 -0400

    Remove Duplicates From Sorted Array

--- src/main/java/LeetCode/ValidParentheses.java
+++ src/main/java/LeetCode/ValidParentheses.java
@@ -1,21 +1,80 @@
 package LeetCode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ValidParentheses {
 
-    /**** method 1 revert half of the ****/
+
     public static boolean isValid(String s) {
-        // the length of string s has to be a even number
-        if (s == null || s.length()%2 != 0) return false;
+
+
+        // the length of string s has to be a even number (or empty)
+
+
+        /**** method 1 revert half of the ****/
+        // mapping
+//        if (s.length()%2 != 0) return false;
 
 
 
+        /**** method 2 use stack ****/
+//        if (s.length()%2 != 0) return false;
+//        char temp = ' ';
+//        Stack<Character> stack = new Stack<Character>();
+//        for (int i = 0 ; i < s.length() ; i++){
+//            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
+//                stack.push(s.charAt(i)); // save the the left side of parentheses to the stack
+//            }
+//            if(s.charAt(i) == ')'){
+//                try { // use try catch in case the stack is empty
+//                    temp = stack.pop();
+//                    if (temp != '(') return false;
+//                } catch (Exception e){
+//                    return false;
+//                }
+//            }
+//            if(s.charAt(i) == ']'){
+//                try {
+//                    temp = stack.pop();
+//                    if(temp != '[') return false;
+//                } catch (Exception e){
+//                    return false;
+//                }
+//            }
+//            if(s.charAt(i) == '}'){
+//                try {
+//                    temp = stack.pop();
+//                    if (temp != '{') return false;
+//                } catch (Exception e){
+//                    return false;
+//                }
+//            }
+//        }
+//
+//        if(stack.size() != 0) return false;
+//
+//        return true;
+
+        /**** updated method 2****/
+        if (s.length()%2 != 0) return false;
+        Stack<Character> stack = new Stack<Character>();
+        for(char c: s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                if(c == '(' ) stack.push(')');
+                if(c == '{' ) stack.push('}');
+                if(c == '[' ) stack.push(']');
+            } else {
+                if(stack.isEmpty()) return false; // right parentheses comes first, which is false
+                if(c != stack.pop()) return false;
+            }
+        }
+        return (stack.empty()) ? true : false; //
     }
 
+
+
     public static void main(String[] args) {
-        String s = "([{";
+        String s = "()";
         System.out.println(isValid(s));
     }
 }
