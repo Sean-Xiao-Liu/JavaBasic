c3b8d2c7eff972576ed451d92d26ed79cfdfca23
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2019-09-29 15:15:05 -0400

    delete node from single linked list

---
+++ src/main/java/DataStructure/SinglyLinkedListNode.java
@@ -0,0 +1,10 @@
+package DataStructure;
+
+public class SinglyLinkedListNode {
+    public int val;
+    public SinglyLinkedListNode next;
+
+    public SinglyLinkedListNode(int val){
+        this.val=val;
+    }
+}
