a300686e1a07381355aa5eab6d67e73c879d9a7c
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2019-11-04 21:28:14 -0500

    add sql practice files

--- src/main/java/OOP/LambdaExpression.java
+++ src/main/java/OOP/LambdaExpression.java
@@ -8,15 +8,23 @@ public class LambdaExpression implements LambdaInterface{
         System.out.println("override");
     }
 
+
+
     public static void main(String[] args) {
         LambdaInterface lambdaInterface = (text) -> System.out.println("lambda expression");
         lambdaInterface.printContent("text");
 
         LambdaInterface lambdaInterface2 = new LambdaExpression();
         lambdaInterface2.printContent("test");
+
+
     }
 }
 
 interface LambdaInterface{
     void printContent(String text);
+
+    static void printText(String text){
+        System.out.println(text);
+    }
 }
---
+++ src/main/resources/db/migration/V1__createTables.sql
@@ -0,0 +1,34 @@
+-- Create tables for data of games, players and teams
+
+CREATE TABLE Games
+(
+    id bigserial  NOT NULL,
+    home_team_id bigint,
+    away_team_id bigint,
+    home_goals smallint,
+    home_losts smallint,
+    home_match_result character varying(50),
+    CONSTRAINT game_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE Players
+(
+    id bigserial NOT NULL,
+    team_id bigint NOT NULL,
+    player_name character varying(50) ,
+    age smallint,
+    player_position character varying(50) ,
+    nationality character varying(50) ,
+
+    CONSTRAINT player_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE Teams
+(
+    id bigserial PRIMARY KEY,
+    team_name character varying(50) ,
+    home_win smallint,
+    away_win smallint,
+    home_loss smallint,
+    away_loss smallint
+);
