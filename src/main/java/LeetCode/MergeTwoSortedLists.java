59272af1c28e29f3f6b9f9bb9c227f02eeabe801
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2019-11-10 17:55:09 -0500

    redo merge sorted list

--- src/main/java/LeetCode/MergeTwoSortedLists.java
+++ src/main/java/LeetCode/MergeTwoSortedLists.java
@@ -29,18 +29,49 @@ public class MergeTwoSortedLists {
     }
 
 
+
+
+
+
+
+
+
+    /*** redo , 11/10/19 ***/
+
+    public static SinglyLinkedListNode mergeTwoLists2(SinglyLinkedListNode l1, SinglyLinkedListNode l2) {
+        SinglyLinkedListNode temp = new SinglyLinkedListNode(0);
+        SinglyLinkedListNode result = temp;
+
+        while(l1 != null && l2 !=null){
+            if(l1.val <= l2.val){
+                temp.next = l1;
+                l1 = l1.next; // move the pointer on l1
+            } else {
+                temp.next = l2;
+                l2 = l2.next; // move the pointer on l2
+            }
+            temp = temp.next; // move the pointer on temp
+        }
+
+        if(l1 != null) temp.next = l1;
+        if(l2 != null) temp.next = l2;
+        return result.next;
+    }
+
+
+
     public static void main(String[] args) {
-        SinglyLinkedListNode l1 = new SinglyLinkedListNode(2);
-        l1.next = new SinglyLinkedListNode(3);
+        SinglyLinkedListNode l1 = new SinglyLinkedListNode(1);
+        l1.next = new SinglyLinkedListNode(2);
         l1.next.next = new SinglyLinkedListNode(4);
-        l1.next.next.next = new SinglyLinkedListNode(5);
+        l1.next.next.next = new SinglyLinkedListNode(8);
 
         SinglyLinkedListNode l2 = new SinglyLinkedListNode(2);
         l2.next = new SinglyLinkedListNode(3);
         l2.next.next = new SinglyLinkedListNode(4);
         l2.next.next.next = new SinglyLinkedListNode(5);
 
-        SinglyLinkedListNode result = mergeTwoLists(l1,l2);
+        SinglyLinkedListNode result = mergeTwoLists2(l1,l2);
         while(result != null){
             System.out.println(result.val);
             result = result.next;
