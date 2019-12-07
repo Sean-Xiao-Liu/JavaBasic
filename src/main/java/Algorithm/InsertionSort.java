d41ca60df31d64b53870ec4f3e73eda9c63a9ae9
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2019-12-07 18:55:49 -0500

    Selection Sort

--- src/main/java/Algorithm/InsertionSort.java
+++ src/main/java/Algorithm/InsertionSort.java
@@ -10,7 +10,7 @@ public class InsertionSort {
             /* Move elements of arr[0..i-1], that are
                greater than key, to one position ahead
                of their current position */
-            while (j >= 0 && arr[j] > key) {
+            while (j >= 0 && arr[j] > key) { // no i in this block, only j
                 arr[j + 1] = arr[j];
                 j = j - 1;
             }
--- src/main/java/Algorithm/SelectionSort.java
+++ src/main/java/Algorithm/SelectionSort.java
@@ -22,9 +22,34 @@ public class SelectionSort {
         }
     }
 
+    public static void selectionSort2(int[] arr){
+        int n = arr.length;
+        for(int i = 0; i < n-1 ; i++){
+            int index = i;
+            for(int j = i+1; j< n;j++){
+                if(arr[j] < arr[index]){
+                    index = j;
+                }
+
+                int temp = arr[i];
+                arr[i] = arr[index];
+                arr[index] = temp;
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
     public static void main(String[] args) {
         int[] test = {3,4,2,1,5,4,3,9,1};
-        selectionSort(test);
+        selectionSort2(test);
 
         for(int j : test){
             System.out.println(j);
