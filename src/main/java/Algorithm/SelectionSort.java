8a679c49af7ac17912d75dfa3c8c2c1057cbb7d8
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2019-12-07 18:25:38 -0500

    Selection Sort

--- src/main/java/Algorithm/SelectionSort.java
+++ src/main/java/Algorithm/SelectionSort.java
@@ -1,4 +1,33 @@
 package Algorithm;
 
 public class SelectionSort {
+    public static void selectionSort(int arr[])
+    {
+        int n = arr.length;
+
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] test = {3,4,2,1,5,4,3,9,1};
+        selectionSort(test);
+
+        for(int j : test){
+            System.out.println(j);
+        }
+    }
 }
