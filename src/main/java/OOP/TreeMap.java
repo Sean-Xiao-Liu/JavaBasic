1f6ffde68d9ae7cee0cd5f0c753de121330657d5
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2019-12-07 18:12:46 -0500

    Insertion Sort

---
+++ src/main/java/OOP/TreeMap.java
@@ -0,0 +1,26 @@
+package OOP;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class TreeMap {
+    public static void main(String[] args) {
+        java.util.TreeMap<String,Integer> treeMap = new java.util.TreeMap<>(Collections.reverseOrder());
+
+        treeMap.put("four",4);
+        treeMap.put("five",5);
+        treeMap.put("one",1);
+        treeMap.put("two",2);
+        treeMap.put("three",3);
+
+        Comparator comp = treeMap.comparator();
+
+
+        System.out.println(treeMap);
+
+    }
+
+}
+
