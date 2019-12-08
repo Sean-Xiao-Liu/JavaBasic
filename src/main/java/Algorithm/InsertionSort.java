7d239fe006965007f77ad5d6566c9fee48fa0070
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2019-12-07 19:00:56 -0500

    Insertion Sort

--- src/main/java/Algorithm/InsertionSort.java
+++ src/main/java/Algorithm/InsertionSort.java
@@ -19,6 +19,36 @@ public class InsertionSort {
         return arr;
     }
 
+
+
+
+
+
+
+    public static void insertionSort2(int[] arr){
+        int n = arr.length;
+        for(int i = 1; i < n ; i++){
+            int key = arr[i];
+            int j = i-1;
+
+            while(j >= 0 && arr[j]>key){
+                arr[j+1] = arr[j];
+                j=j-1;
+
+            }
+            arr[j+1] = key;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
     public static void main(String[] args) {
         int[] test = {5,2,3,4,1};
         int[] result = insertionSort(test);
