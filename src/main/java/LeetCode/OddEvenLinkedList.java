98199dfedb18a95da71d69eb7866c4568ce7ac24
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2019-12-12 21:11:20 -0500

    Odd Even Linked List

--- src/main/java/LeetCode/OddEvenLinkedList.java
+++ src/main/java/LeetCode/OddEvenLinkedList.java
@@ -2,8 +2,22 @@ package LeetCode;
 
 public class OddEvenLinkedList {
     public ListNode oddEvenList(ListNode head) {
+        // boundary cases
         if(head == null ) return null;
         if(head.next == null) return head;
-        ListNode
+
+
+        ListNode odd = head; // mark the odd part
+        ListNode even = head.next;
+        ListNode evenHead = even; // mark the head of even part
+        while (even != null && even.next != null) { // can't use
+            odd.next = even.next; // eg. 1 -> 3
+            odd = odd.next; // move the odd pointer
+            even.next = odd.next; // eg. 2 -> 4
+            even = even.next; // move the even pointer
+        }
+
+        odd.next = evenHead; // concat the odd part with the even part
+        return head;
     }
 }
