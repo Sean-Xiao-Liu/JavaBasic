cf8ed7cae8987991137b7ff5a5cf34d8a56dae48
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2019-12-20 14:20:14 -0500

    Permutation

--- src/main/java/LeetCode/Permutations.java
+++ src/main/java/LeetCode/Permutations.java
@@ -14,21 +14,24 @@ public class Permutations {
         return list;
     }
 
-    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
-        if(tempList.size() == nums.length){
+    private static void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
+        if(tempList.size() == nums.length){ // when each temp list contains all numbers, add the temp list to list
             list.add(new ArrayList<>(tempList));
+//            list.add(tempList); //will make all temp list empty
         } else{
             for(int i = 0; i < nums.length; i++){
-                if(tempList.contains(nums[i])) continue; // element already exists, skip
+                if(tempList.contains(nums[i])) continue; // element already exists, skip the rest operation and i++
                 tempList.add(nums[i]);
                 backtrack(list, tempList, nums);
-                tempList.remove(tempList.size() - 1);
+                tempList.remove(tempList.size() - 1); // remove the last index of temp list, after the temp list added to the list, back to this step
+
             }
         }
     }
 
     public static void main(String[] args) {
         int[] array = {1,2,3};
+        System.out.println(permute(array));
 
     }
 }
