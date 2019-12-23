877050d269364c882cb209309f9d1a1743f87408
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2019-12-23 15:32:10 -0500

    Stream API

--- src/main/java/CoreJava/StreamAPI.java
+++ src/main/java/CoreJava/StreamAPI.java
@@ -5,14 +5,16 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class StreamAPI {
 
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
-        System.out.println("result of external iterating " + sumIterator(list));
-        System.out.println("result of external iterating " + sumStream(list));
-        System.out.println(getSquaresList(list));
+//        List<Integer> list = Arrays.asList(3, 2, 2, 3, 7, 3, 5,11,12);
+//        System.out.println("result of external iterating " + sumIterator(list));
+//        System.out.println("result of external iterating " + sumStream(list));
+//        System.out.println(getSquaresList(list));
+        System.out.println("sum of even number smaller than 10 is " + sumEvenNumber(10));
 
     }
 
@@ -44,7 +46,14 @@ public class StreamAPI {
         return squaresList;
     }
 
+    private static int sumEvenNumber(int n){
+        return IntStream
+                .range(1,n+1)
+                .filter(i -> i%2 == 0)
+//                .map(i -> i * i)
+                .sum();
 
+    }
 
 
 }
