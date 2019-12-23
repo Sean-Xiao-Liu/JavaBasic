afd301416ab1b29708450a4085dbbded3b878a5e
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2019-12-22 21:03:18 -0500

    Swap without Temp

--- src/main/java/CoreJava/SwapWithoutTemp.java
+++ src/main/java/CoreJava/SwapWithoutTemp.java
@@ -1,4 +1,17 @@
 package CoreJava;
 
 public class SwapWithoutTemp {
+    public static void swapString(String a,String b){
+        System.out.println("Before swap: " + a + " " + b);
+        a = a + b;
+        b = a.substring(0, a.length() - b.length()); // take start and end index
+        a = a.substring(b.length()); // take start index
+        System.out.println("After : " + a + " " + b);
+    }
+
+    public static void main(String[] args) {
+        String a = "Love";
+        String b = "You";
+        swapString(a,b);
+    }
 }
