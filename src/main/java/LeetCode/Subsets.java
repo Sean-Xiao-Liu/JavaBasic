d527865e6c7b7ce997a11cfdab0b486912137357
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2019-12-22 21:02:07 -0500

    Subsets

--- src/main/java/LeetCode/Subsets.java
+++ src/main/java/LeetCode/Subsets.java
@@ -20,7 +20,7 @@ public class Subsets {
     }
 
     public static void main(String[] args) {
-        int[] nums = {2,1,3,4};
+        int[] nums = {2,1,3,4,4};
         System.out.println(subsets(nums));
     }
 }
