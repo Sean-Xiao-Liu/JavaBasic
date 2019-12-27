d461e01b773c8496136d0b6a0cd7a189260e8d3f
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2019-12-27 16:19:53 -0500

    Interface Example

---
+++ src/main/java/CoreJava/InterfaceExample.java
@@ -0,0 +1,47 @@
+package CoreJava;
+
+public class InterfaceExample implements PrintString,AnotherPrintString{
+
+
+
+    public static void main(String[] args) {
+//        PrintString printString = new PrintString();
+
+        InterfaceExample interfaceExample = new InterfaceExample();
+        interfaceExample.printAbstract();
+        interfaceExample.printDefault();
+        PrintString.printStatic();
+    }
+
+    @Override
+    public void printAbstract() {
+        System.out.println("override abstract method");
+    }
+
+    @Override
+    public void printDefault(){
+        System.out.println("print updated default method");
+    }
+
+
+    @Override
+    public void anotherPrintAbstract() {
+        System.out.println("override another abstract method");
+    }
+}
+
+interface PrintString{
+    void printAbstract();
+
+    default void printDefault(){
+        System.out.println("print default method");
+    }
+
+    static void printStatic(){
+        System.out.println("print static method");
+    }
+}
+
+interface AnotherPrintString{
+    void anotherPrintAbstract();
+}
