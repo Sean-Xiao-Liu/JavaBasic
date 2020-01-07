8a14c998aecb4997555d33a96bc6566013a5a701
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-01-07 14:34:54 -0500

    Add color to multi thread

--- src/main/java/CoreJava/MultiThead.java
+++ src/main/java/CoreJava/MultiThead.java
@@ -1,5 +1,7 @@
 package CoreJava;
 
+import static CoreJava.ThreadColor.*;
+
 public class MultiThead extends Thread{
     @Override
     public void run() {
@@ -20,7 +22,7 @@ public class MultiThead extends Thread{
         MultiThead m4 = new MultiThead();
         m3.setPriority(10);
         m4.setPriority(1);
-        System.out.println("Thread m1 begin");
+        System.out.println(ANSI_GREEN+"Thread m1 begin");
         m1.start();
 //        m1.start(); // After starting a thread, it can never be started again. If you does so, an IllegalThreadStateException is thrown. In such case, thread will run once but for second time, it will throw exception.
         try{
@@ -28,11 +30,11 @@ public class MultiThead extends Thread{
         }catch(Exception e){
             System.out.println(e);
         }
-        System.out.println("Thread m2 begin");
+        System.out.println(ANSI_BLUE+"Thread m2 begin");
         m2.start();
-        System.out.println("Thread m3 begin");
+        System.out.println(ANSI_RED+"Thread m3 begin");
         m3.start();
-        System.out.println("Thread m4 begin");
+        System.out.println(ANSI_PURPLE+"Thread m4 begin");
         m4.start();
 
     }
