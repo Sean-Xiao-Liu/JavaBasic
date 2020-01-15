635c60d3fc6d13acbfba96f1059ed547a0865b6d
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-01-14 19:53:23 -0500

    unique paths

--- src/main/java/CoreJava/UniquePaths.java
+++ src/main/java/CoreJava/UniquePaths.java
@@ -1,4 +1,27 @@
 package CoreJava;
+/*
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+Example 1:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Right -> Down
+2. Right -> Down -> Right
+3. Down -> Right -> Right
+Example 2:
+
+Input: m = 7, n = 3
+Output: 28
+
+*/
+
 public class UniquePaths {
     public static int uniquePaths(int m, int n) {
         int[][] matrix = new int[m][n]; //construct a matrix to reflex possible move to each position
