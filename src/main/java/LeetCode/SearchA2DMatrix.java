5890f4d53b6d90654d4b4254c93aec0d2de5c56f
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-01-28 13:37:11 -0500

    Search a 2D Matrix add comments and boundary case

--- src/main/java/LeetCode/SearchA2DMatrix.java
+++ src/main/java/LeetCode/SearchA2DMatrix.java
@@ -21,6 +21,7 @@ public class SearchA2DMatrix {
     //method 2
     public static boolean searchMatrix(int[][] matrix, int target) {
         boolean result = false;
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
 
         int j = matrix.length-1;
         for (int i = 0; i < matrix.length; i++) {
@@ -30,24 +31,20 @@ public class SearchA2DMatrix {
             }
         }
 
-//        int row = findRow(matrix,target);
+        // binary search to find target
         int low = 0;
-//        int high = matrix[row].length - 1;
         int high = matrix[j].length - 1;
 
         while (low <= high) {
             int mid = (low + high) / 2;
-//            System.out.println("the mid position is " + mid);
             int guess = matrix[j][mid];
             if (guess == target) {
                 result = true;
             }
             if (guess > target) {
                 high = mid - 1;
-//                high =  mid;// may lead to Infinite loop
             } else {
                 low = mid + 1;
-//                low = mid; // may lead to Infinite loop
             }
         }
 
@@ -67,13 +64,15 @@ public class SearchA2DMatrix {
 
 
     public static void main(String[] args) {
-        int target = 10;
-        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        int target = 5;
+//        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+          int[][] matrix = {{}};
 //        int result = findRow(matrix,target);
 //        System.out.println(result);
 //        System.out.println(matrix[1][1]);
 //        System.out.println(matrix.length);
-
+        System.out.println(matrix.length);
+        System.out.println(matrix[0].length);
         System.out.println(searchMatrix(matrix,target));
     }
 }
