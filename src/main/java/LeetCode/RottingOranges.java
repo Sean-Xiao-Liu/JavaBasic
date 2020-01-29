13b988b855f71b13db6654ff51780af105e78806
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-01-29 10:53:56 -0500

    Rotting Oranges BFS

--- src/main/java/LeetCode/RottingOranges.java
+++ src/main/java/LeetCode/RottingOranges.java
@@ -51,7 +51,7 @@ public class RottingOranges {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 1 || grid[i][j] == 2) total++; // count total number of oranges
-                if (grid[i][j] == 2) q.offer(new Position(i, j));  
+                if (grid[i][j] == 2) q.offer(new Position(i, j));
             }
         }
 
