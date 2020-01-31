703fa02c390c3178580ef67bc35a6967c5bf718e
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-01-31 14:54:16 -0500

    Copy List with Random Pointer: Recursion

--- src/main/java/LeetCode/CopyListWithRandomPointer.java
+++ src/main/java/LeetCode/CopyListWithRandomPointer.java
@@ -1,4 +1,103 @@
 package LeetCode;
 
+/*A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+
+Return a deep copy of the list.
+
+The Linked List is represented in the input/output as a list of n nodes. Each node is represented as a pair of [val, random_index] where:
+
+val: an integer representing Node.val
+random_index: the index of the node (range from 0 to n-1) where random pointer points to, or null if it does not point to any node.
+
+
+Example 1:
+
+
+Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
+Example 2:
+
+
+Input: head = [[1,1],[2,1]]
+Output: [[1,1],[2,1]]
+Example 3:
+
+
+
+Input: head = [[3,null],[3,0],[3,null]]
+Output: [[3,null],[3,0],[3,null]]
+Example 4:
+
+Input: head = []
+Output: []
+Explanation: Given linked list is empty (null pointer), so return null.
+
+
+Constraints:*/
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class CopyListWithRandomPointer {
+
+    /**method 1 recursive**/
+    // HashMap which holds old nodes as keys and new nodes as its values.
+    Map<Node, Node> visitedHash = new HashMap<Node, Node>();
+
+    public Node copyRandomList(Node head) {
+
+        if (head == null) {
+            return null;
+        }
+
+        // If we have already processed the current node, then we simply return the cloned version of
+        // it.
+        if (this.visitedHash.containsKey(head)) {
+            return this.visitedHash.get(head);
+        }
+
+        // Create a new node with the value same as old node. (i.e. copy the node)
+        Node node = new Node(head.val);
+
+        // Save this value in the hash map. This is needed since there might be
+        // loops during traversal due to randomness of random pointers and this would help us avoid
+        // them.
+        this.visitedHash.put(head, node);
+
+        // Recursively copy the remaining linked list starting once from the next pointer and then from
+        // the random pointer.
+        // Thus we have two independent recursive calls.
+        // Finally we update the next and random pointers for the new node created.
+        node.next = this.copyRandomList(head.next);
+        node.random = this.copyRandomList(head.random);
+
+        return node;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.random = head.next;
+        head.next.random = head;
+        head.next.next.random =  head.next;
+
+
+        while(head != null){
+            System.out.println("The node value is  " + head.val);
+            System.out.println("The node random index is  " + head.random.val);
+            head = head.next;
+        }
+    }
+}
+
+class Node{
+    int val;
+    Node next;
+    Node random;
+    public Node(int val){
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
 }
