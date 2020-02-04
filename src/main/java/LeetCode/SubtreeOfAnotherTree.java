519398f847881b66cf36737d6eb43b3afe04ed2f
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-02-04 16:54:41 -0500

    Subtree of another tree: recursive

---
+++ src/main/java/LeetCode/SubtreeOfAnotherTree.java
@@ -0,0 +1,52 @@
+package LeetCode;
+
+//import DataStructure.TreeNode;
+
+public class SubtreeOfAnotherTree {
+    public static boolean isSubtree(TreeNode s, TreeNode t) {
+        String stringS = preorder(s,true);
+        String stringT = preorder(t,true);
+        return stringS.indexOf(stringT) >= 0;
+    }
+
+    // use recursive method to distinguish the null value on the left and right null
+    public static String preorder(TreeNode t, boolean left) {
+        if (t == null) {
+            if (left)
+                return "lnull";
+            else
+                return "rnull";
+        }
+        return "#"+t.val + " " +preorder(t.left, true)+" " +preorder(t.right, false);
+    }
+
+
+
+    public static void main(String[] args) {
+        TreeNode s = new TreeNode(3);
+        s.left = new TreeNode(4);
+        s.right = new TreeNode(5);
+        s.left.left = new TreeNode(1);
+        s.left.right =  new TreeNode(2);
+//        s.left.right.left =  new TreeNode(0);
+
+
+        TreeNode t = new TreeNode(4);
+        t.left = new TreeNode(1);
+        t.right = new TreeNode(2);
+
+        System.out.println(isSubtree(s,t));
+        System.out.println(preorder(s,true));
+        System.out.println(preorder(t,true));
+    }
+
+}
+
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val){
+        this.val = val;
+    }
+}
