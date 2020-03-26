c64f03aa86fa873909141a7061503d4f7734a9b0
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-03-26 09:59:56 -0400

    add problem description

--- src/main/java/LeetCode/PalindromeNumber.java
+++ src/main/java/LeetCode/PalindromeNumber.java
@@ -1,9 +1,23 @@
 package LeetCode;
 
+/**
+ * Example 1:
+ *
+ * Input: 121
+ * Output: true
+ * Example 2:
+ *
+ * Input: -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+ * Example 3:
+ *
+ * Input: 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ * **/
+
 public class PalindromeNumber {
-
-
-
     public boolean isPalindrome(int x) {
 //    //method 1 trans int to string, traversal and compare the head and tail of the string
 //    boolean ans = true;
