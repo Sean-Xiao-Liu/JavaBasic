c23e29ba9637d1045c111f29a400a8b6704972d5
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-04-07 21:02:00 -0400

    try extended constructor

--- src/main/java/CoreJava/Constructor.java
+++ src/main/java/CoreJava/Constructor.java
@@ -6,39 +6,53 @@ public class Constructor {
 
     public String word;
 
-    public Constructor(int num, String word){ // sample of constructor
+    public Constructor(int num, String word) { // sample of constructor
         this.number = num;
         this.word = word;
     }
 
+    // add another constructor to the class
+    // a class can have more than one constructor, invoke one as needed when initialize the object
     public Constructor() {
+//        this.number = 10000;
+        this.word = "this is constructor without argument";
 
     }
 
 
     public static void main(String[] args) {
-        Constructor test = new Constructor(1,"this is a sample of constructor");
+        Constructor test = new Constructor(1, "this is a sample of constructor");
+        Constructor test3 = new Constructor();
         System.out.println(test.number);
         System.out.println(test.word);
+        System.out.println(test3.number);
+        System.out.println(test3.word);
 
-        AnotherConstructor test2 = new AnotherConstructor(1,"this is a sample of constructor", 2);
+        AnotherConstructor test2 = new AnotherConstructor(1111, "this is a sample of sub constructor", 2);
         System.out.println(test2.number);
+        System.out.println(test2.word);
+        System.out.println(test2.anotherNumber);
+        AnotherConstructor test4 = new AnotherConstructor();
+        System.out.println(test4.word);
+        System.out.println(test4.number);
+        System.out.println(test4.anotherNumber);
     }
 }
 
-class AnotherConstructor extends Constructor{
+class AnotherConstructor extends Constructor {
 
     public int anotherNumber;
 
 
-    public AnotherConstructor(int num, String word) {
-        super(num, word);
+    public AnotherConstructor() {
+        super(); // invoke the non-argument constructor
+        this.anotherNumber = 333;
     }
 
-    public AnotherConstructor(int num, String word, int anotherNumber){
-        super();
-        this.word = word;
-        this.number = num;
+    public AnotherConstructor(int num, String word, int anotherNumber) {
+//        super();
+        this.word = super.word; // will take the word of super constructor
+        this.number = super.number;
         this.anotherNumber = anotherNumber;
 
     }
