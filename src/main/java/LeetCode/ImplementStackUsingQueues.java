197d898f7aee5045851146c2bc432d24ded6a295
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-04-12 14:52:59 -0400

    add comments

--- src/main/java/LeetCode/ImplementStackUsingQueues.java
+++ src/main/java/LeetCode/ImplementStackUsingQueues.java
@@ -18,13 +18,15 @@ public class ImplementStackUsingQueues {
 
     /** Push element x onto stack. */
     public void push(int x) {
+
+        // make sure the new added number x is always at the front
         q2.add(x);
         top = x;
         while (!q1.isEmpty()) {
             q2.add(q1.remove());
         }
 
-        //swap q1 and q2
+        //swap q1 and q2 to make sure the q2 is always an empty queue
         Queue<Integer> temp = q1;
         q1 = q2;
         q2 = temp;
