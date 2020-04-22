a3dc9bd418656a868a275671578be0f36ff04df5
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-04-21 21:38:00 -0400

    use comparator and linkedlist to merge intervals

---
+++ src/main/java/LeetCode/MergeIntervals.java
@@ -0,0 +1,163 @@
+package LeetCode;
+
+
+import java.util.*;
+
+/**
+ * Given a collection of intervals, merge all overlapping intervals.
+ *
+ * Example 1:
+ *
+ * Input: [[1,3],[2,6],[8,10],[15,18]]
+ * Output: [[1,6],[8,10],[15,18]]
+ * Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+ * Example 2:
+ *
+ * Input: [[1,4],[4,5]]
+ * Output: [[1,5]]
+ * Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+ * **/
+public class MergeIntervals {
+
+    /**
+     * method 1
+     **/
+    private Map<int[], List<int[]>> graph;
+    private Map<Integer, List<int[]>> nodesInComp;
+    private Set<int[]> visited;
+
+    // return whether two intervals overlap (inclusive)
+    private boolean overlap(int[] a, int[] b) {
+        return a[0] <= b[1] && b[0] <= a[1];
+    }
+
+    // build a graph where an undirected edge between intervals u and v exists
+    // if u and v overlap.
+    private void buildGraph(int[][] intervals) {
+        graph = new HashMap<>();
+        for (int[] interval : intervals) {
+            graph.put(interval, new LinkedList<>()); // give every interval a corresponding list
+        }
+
+        for (int[] interval1 : intervals) {
+            for (int[] interval2 : intervals) {
+                if (overlap(interval1, interval2)) { // invoke overlap method, put all overlapped intervals of 1 interval into the list
+                    graph.get(interval1).add(interval2);
+                    graph.get(interval2).add(interval1);
+                }
+            }
+        }
+    }
+
+    // merges all of the nodes in this connected component into one interval.
+    // find the smallest and largest number in the list since they both overlapped
+    private int[] mergeNodes(List<int[]> nodes) {
+        int minStart = nodes.get(0)[0];
+        for (int[] node : nodes) {
+            minStart = Math.min(minStart, node[0]);
+        }
+
+        int maxEnd = nodes.get(0)[1];
+        for (int[] node : nodes) {
+            maxEnd = Math.max(maxEnd, node[1]);
+        }
+
+        return new int[]{minStart, maxEnd};
+    }
+
+    // use depth-first search to mark all nodes in the same connected component
+    // with the same integer.
+    private void markComponentDFS(int[] start, int compNumber) {
+        Stack<int[]> stack = new Stack<>();
+        stack.add(start);
+
+        while (!stack.isEmpty()) {
+            int[] node = stack.pop();
+            if (!visited.contains(node)) {
+                visited.add(node);
+
+                if (nodesInComp.get(compNumber) == null) {
+                    nodesInComp.put(compNumber, new LinkedList<>());
+                }
+                nodesInComp.get(compNumber).add(node);
+
+                for (int[] child : graph.get(node)) {
+                    stack.add(child);
+                }
+            }
+        }
+    }
+
+    // gets the connected components of the interval overlap graph.
+    private void buildComponents(int[][] intervals) {
+        nodesInComp = new HashMap<>();
+        visited = new HashSet<>();
+        int compNumber = 0;
+
+        for (int[] interval : intervals) {
+            if (!visited.contains(interval)) {
+                markComponentDFS(interval, compNumber);
+                compNumber++;
+            }
+        }
+    }
+
+    public int[][] merge(int[][] intervals) {
+        buildGraph(intervals);
+        buildComponents(intervals);
+
+        // for each component, merge all intervals into one interval.
+        List<int[]> merged = new LinkedList<>();
+        for (int comp = 0; comp < nodesInComp.size(); comp++) {
+            merged.add(mergeNodes(nodesInComp.get(comp)));
+        }
+
+        return merged.toArray(new int[merged.size()][]);
+    }
+
+    /**
+     * method 2
+     **/
+
+
+        public int[][] merge2(int[][] intervals) {
+            Collections.sort(Arrays.asList(intervals), new IntervalComparator());
+
+            LinkedList<int[]> merged = new LinkedList<>(); // use linkedlist here since it implements the deque interface and can invoke getLast method without traversal
+            for (int[] interval : intervals) {
+                // if the list of merged intervals is empty or if the current
+                // interval does not overlap with the previous, simply append it.
+                if (merged.isEmpty() || merged.getLast()[1] < interval[0]) {
+                    merged.add(interval);
+                }
+                // otherwise, there is overlap, so we merge the current and previous
+                // intervals.
+                else {
+                    merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+                }
+            }
+
+            return merged.toArray(new int[merged.size()][]); // convert a list of array into a 2d array, invoke toArray method and give size of 2 d array
+        }
+
+
+    public static void main(String[] args) {
+        int[][] ints = {{8, 10}, {15, 18}, {1, 3}, {2, 6},};
+        MergeIntervals test = new MergeIntervals();
+        int[][] result = test.merge2(ints);
+    }
+}
+
+class IntervalComparator implements Comparator<int[]> {
+    @Override
+    public int compare(int[] a, int[] b) {
+//            return a[0] < b[0] ? -1 : a[0] == b[0] ? 0 : 1;
+        if (a[0] < b[0]) {
+            return -1;
+        } else if (a[0] == b[0]){
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
