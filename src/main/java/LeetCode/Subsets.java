5120134856388b12af2111169a99e3da0efae900
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-07-01 15:53:16 -0400

    add comments on loop

--- src/main/java/LeetCode/Subsets.java
+++ src/main/java/LeetCode/Subsets.java
@@ -12,7 +12,7 @@ public class Subsets {
 
     private static void backtrack(List<List<Integer>> list, List<Integer> temp, int[] nums,int start){
         list.add(new ArrayList<>(temp)); // no limit on the length of temp list, so just add whatever it have to the list
-        for(int i = start; i < nums.length; i++){
+        for(int i = start; i < nums.length; i++){ // remember the loop start with "start" not zero or lead to overflow
             temp.add(nums[i]);
             backtrack(list, temp, nums, i + 1);
             temp.remove(temp.size() - 1);
@@ -20,7 +20,7 @@ public class Subsets {
     }
 
     public static void main(String[] args) {
-        int[] nums = {0,1,2,3,4,5,6,7,8,9};
+        int[] nums = {1,2,3,4};
         System.out.println(subsets(nums));
         System.out.println("size of possible combine " + subsets(nums).get(5));
     }
