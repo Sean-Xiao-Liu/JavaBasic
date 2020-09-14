88271461cda2b22216fab4e952f25379dad02667
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-09-14 15:29:01 -0400

    Create custom annotations

--- src/main/java/CoreJava/Annotation.java
+++ src/main/java/CoreJava/Annotation.java
@@ -1,4 +1,17 @@
 package CoreJava;
 
+@MyOwnAnnotation(value = 10)
+@MyAnnotation2(value2 = "value2", value3 = "value3")
 public class Annotation {
+}
+
+
+@interface MyOwnAnnotation{
+    int value();
+}
+
+@interface MyAnnotation2{
+    int value1() default 0;
+    String value2();
+    String value3();
 }
