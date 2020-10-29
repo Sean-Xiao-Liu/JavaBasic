196c5401b8a6329354791e8874ff2b3213d16270
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-10-29 14:43:22 -0400

    add recursive method and update comment for interative method

--- src/main/java/LeetCode/ReverseLinkedList.java
+++ src/main/java/LeetCode/ReverseLinkedList.java
@@ -3,11 +3,13 @@ package LeetCode;
 import DataStructure.SinglyLinkedListNode;
 
 public class ReverseLinkedList {
-    public static SinglyLinkedListNode reverse(SinglyLinkedListNode head){
-        SinglyLinkedListNode prev = null;
-        SinglyLinkedListNode curr = head;
+
+    /** method 1 iterative method */
+    public static ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
         while(curr != null){
-            SinglyLinkedListNode nextTemp = curr.next;
+            ListNode nextTemp = curr.next;
             curr.next = prev;
             prev = curr;
             curr = nextTemp;
@@ -15,11 +17,23 @@ public class ReverseLinkedList {
         return prev;
     }
 
+    /** method 2 recursive method */
+    public static ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head; // boundary case
+        ListNode temp = reverseList(head.next); // mark the node to be returned，先一直走到最后一个node，再倒回来执行下一行
+
+        head.next.next = head;
+        head.next = null;
+        return temp;
+    }
+
     public static void main(String[] args) {
-        SinglyLinkedListNode root = new SinglyLinkedListNode(1);
-        root.next = new SinglyLinkedListNode(2);
-        root.next.next = new SinglyLinkedListNode(3);
-        SinglyLinkedListNode result = reverse(root);
+        ListNode root = new ListNode(1);
+        root.next = new ListNode(2);
+        root.next.next = new ListNode(3);
+        root.next.next.next = new ListNode(4);
+//        ListNode result = reverse(root);
+        ListNode result = reverseList(root);
 
         while(result != null){
             System.out.println(result.val);
