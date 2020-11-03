e6e19ffe3e52cd053cbacb6a65bb603cdc1b179a
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-11-03 15:03:09 -0500

    use recursion and DFS

---
+++ src/main/java/LeetCode/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,27 @@
+package LeetCode;
+
+public class MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int left_height = maxDepth(root.left);
+            int right_height = maxDepth(root.right);
+            return java.lang.Math.max(left_height, right_height) + 1;
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(6);
+        root.right = new TreeNode(3);
+        root.right.right = new TreeNode(4);
+        root.right.right.left = new TreeNode(5);
+
+        MaximumDepthOfBinaryTree test = new MaximumDepthOfBinaryTree();
+        System.out.println(test.maxDepth(root));
+    }
+}
