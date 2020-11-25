7ac11c9b8ca636b166ea50a8801ac24286e962d0
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2020-11-25 12:06:06 -0500

    add tree nodes demo

--- src/main/java/LeetCode/BinaryTreeInorderTraversal.java
+++ src/main/java/LeetCode/BinaryTreeInorderTraversal.java
@@ -1,7 +1,5 @@
 package LeetCode;
 
-import DataStructure.TreeNode;
-;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -62,14 +60,32 @@ public class BinaryTreeInorderTraversal {
 
 
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(5);
-        root.right = new TreeNode(2);
-        root.right.left = new TreeNode(3);
-        root.right.left.right = new TreeNode(4);
+        LeetCode.TreeNode root = new LeetCode.TreeNode(1); // the first node with value 1
+        LeetCode.TreeNode left = new LeetCode.TreeNode(2);
+        LeetCode.TreeNode right = new LeetCode.TreeNode(3);
+        root.left = left; // connect the left node with the root node
+        root.right = right;
+        left.left = new LeetCode.TreeNode(4);
+        left.right = new LeetCode.TreeNode(5);
+        right.left = new LeetCode.TreeNode(6);
+        right.right = new LeetCode.TreeNode(7);
+        left.left.left = new LeetCode.TreeNode(8);
+        left.left.right = new LeetCode.TreeNode(9);
+        left.right.right = new LeetCode.TreeNode(10);
+        right.right.left = new LeetCode.TreeNode(11);
+        /*
+         *  the tree looks like:
+         *             1
+         *           /   \
+         *          2     3
+         *        /  \   /  \
+         *       4    5 6    7
+         *      / \    \    /
+         *     8   9    10 11
+         * */
 
-        System.out.println(inorderTraversal(root) );
-        System.out.println(inorderTraversal2(root));
+
+        System.out.println(inorderTraversal3(root));
 
     }
 }
