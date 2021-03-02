62e644378a5795e337a4306df25ebe300f85d7d7
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2021-03-02 15:50:50 -0500

    use regex to replace extra spaces in the string / split string by spaces

---
+++ src/main/java/CoreJava/Regex.java
@@ -0,0 +1,31 @@
+package CoreJava;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex {
+    public static String replaceMoreThanOneSpaces(String string){
+//        String pattern = "  ";
+        String pattern = " +"; // n+ Matches any string that contains at least one n,it match more than one spaces
+        string = string.replaceAll(pattern," ");
+        return string;
+
+    }
+
+    public static String[] splitBySpaces(String string){
+        String pattern = " +";
+        String[] strings = string.split(pattern);
+        return strings;
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println(replaceMoreThanOneSpaces("Two              spaces"));
+        String[] strings = splitBySpaces("One Two   Three       Four");
+        for(String s:strings){
+            System.out.println(s);
+        }
+        
+    }
+}
