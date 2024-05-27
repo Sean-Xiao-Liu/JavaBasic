96f4ba4a86fee802aba0e7a27891d96874c80c98
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2024-05-26 22:01:44 -0400

    turn string into char array to track invalid parentheses

--- src/main/java/LeetCode/MinimumRemoveToMakeValidParentheses.java
+++ src/main/java/LeetCode/MinimumRemoveToMakeValidParentheses.java
@@ -3,43 +3,79 @@ package LeetCode;
 import java.util.Stack;
 
 public class MinimumRemoveToMakeValidParentheses {
+//    public static String minRemoveToMakeValid(String s) {
+//        int length = s.length();
+//        Stack<Character> leftStack = new Stack<>();
+//        Stack<Character> rightStack = new Stack<>();
+//        StringBuilder result = new StringBuilder();
+//
+//        for(int i = 0 ; i < length; i++){
+//            if (s.charAt(i) == '('){
+//                leftStack.push(s.charAt(i));
+//                result.append(s.charAt(i));
+//            } else if (s.charAt(i) == ')'){
+//                try{
+//                    if (leftStack.peek() == '('){
+//                        leftStack.pop();
+//                        result.append(s.charAt(i));
+//                    }
+//                } catch (Exception e){
+//
+//                }
+//            } else {
+//                result.append(s.charAt(i));
+//            }
+//        }
+//
+//        for(int j = result.length()-1 ; j >= 0; j--){
+//            if (result.charAt(j) == ')'){
+//                rightStack.push(result.charAt(j));
+//            }
+//            if (result.charAt(j) == '('){
+//                try{
+//                    if (rightStack.peek() == ')'){
+//                        rightStack.pop();
+//                    }
+//                } catch (Exception e){
+//                    result.deleteCharAt(j);
+//                }
+//            }
+//        }
+//
+//        return result.toString();
+//    }
+
     public static String minRemoveToMakeValid(String s) {
-        int length = s.length();
-        Stack<Character> leftStack = new Stack<>();
-        Stack<Character> rightStack = new Stack<>();
-        StringBuilder result = new StringBuilder();
+        char[] chars = s.toCharArray();
+        int openCount = 0;
 
-        for(int i = 0 ; i < length; i++){
-            if (s.charAt(i) == '('){
-                leftStack.push(s.charAt(i));
-                result.append(s.charAt(i));
-            } else if (s.charAt(i) == ')'){
-                try{
-                    if (leftStack.peek() == '('){
-                        leftStack.pop();
-                        result.append(s.charAt(i));
-                    }
-                } catch (Exception e){
-
+        // Forward pass: mark invalid closing parentheses and count open parentheses
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (c == '(') {
+                openCount++;
+            } else if (c == ')') {
+                if (openCount == 0) {
+                    chars[i] = '*'; // Mark invalid closing parenthesis
+                } else {
+                    openCount--;
                 }
-            } else {
-                result.append(s.charAt(i));
             }
         }
 
-        for(int j = result.length()-1 ; j >= 0; j--){
-            if (result.charAt(j) == ')'){
-                rightStack.push(result.charAt(j));
-            } else if (result.charAt(j) == '('){
-                try{
-                    if (rightStack.peek() == ')'){
-                        rightStack.pop();
-                    }
-                } catch (Exception e){
-                    result.deleteCharAt(j);
-                }
-            } else {
+        // Backward pass: mark extra open parentheses
+        for (int i = chars.length - 1; i >= 0 && openCount > 0; i--) {
+            if (chars[i] == '(') {
+                chars[i] = '*'; // Mark extra open parenthesis
+                openCount--;
+            }
+        }
 
+        // Construct result string
+        StringBuilder result = new StringBuilder();
+        for (char c : chars) {
+            if (c != '*') {
+                result.append(c);
             }
         }
 
