210f119e45fb42900623a2be8e996e748723ee57
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2024-09-16 22:55:05 -0400

    leetCode blind 75

--- src/main/java/LeetCode/MaximumDepthOfBinaryTree.java
+++ src/main/java/LeetCode/MaximumDepthOfBinaryTree.java
@@ -1,6 +1,9 @@
 package LeetCode;
 
+import DataStructure.Queue;
 import DataStructure.TreeNode;
+
+import java.util.LinkedList;
 
 public class MaximumDepthOfBinaryTree {
     public int maxDepth(TreeNode root) {
@@ -9,21 +12,42 @@ public class MaximumDepthOfBinaryTree {
         } else {
             int left_height = maxDepth(root.left);
             int right_height = maxDepth(root.right);
-            return java.lang.Math.max(left_height, right_height) + 1;
+            return Math.max(left_height, right_height) + 1;
         }
     }
 
+    public int maxDepth2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int count = 0;
+        LinkedList<TreeNode> queue = new LinkedList<>();
+
+        queue.push(root);
+
+        while (!queue.isEmpty()) {
+            // get number of nodes in current level
+            int levelSize = queue.size();
+            count++;
+
+            // only process certain number of node, which is the levelSize
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+        }
+        return count;
+    }
 
 
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.left.left = new TreeNode(6);
-        root.right = new TreeNode(3);
-        root.right.right = new TreeNode(4);
-        root.right.right.left = new TreeNode(5);
+        root.left = new TreeNode(2,new TreeNode(4),null);
+        root.right = new TreeNode(3,null,new TreeNode(5));
+
 
         MaximumDepthOfBinaryTree test = new MaximumDepthOfBinaryTree();
-        System.out.println(test.maxDepth(root));
+        System.out.println(test.maxDepth2(root));
     }
 }
